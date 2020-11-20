82c3e6479a66049a5c3858bf9813ba7e3c06915a
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-11-20 23:18:12 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan58.tambahkurang;
+
+/**
+ *
+ * @author RizalSihombing
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan penjumlahan dan pengurangan
+ */
+public class JumlahBilangan extends Bilangan{
+    
+    public void tampilHasilJumlah() {
+        System.out.println("Hasil Penjumlahan = " + (getX() + getY()));
+    }
+}
